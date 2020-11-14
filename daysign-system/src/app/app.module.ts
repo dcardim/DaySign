96f299586587c0ceea79840b0e75eec295d1d38b
Dayana Junger <dayana.junger@senac.br>
2020-11-14 20:34:04 -0300

    first commit

---
+++ daysign-system/src/app/app.module.ts
@@ -0,0 +1,22 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { ButtonComponent } from './app/componentes/button/button.component';
+import { ComponentsComponent } from './app/components/components.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    ButtonComponent,
+    ComponentsComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
